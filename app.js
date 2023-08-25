45e4349ae0c27cacb1fce55d519ce20b536bdf70
Mohammed Yaseen <mohammed.yaseen@kalvium.community>
2023-08-25 18:59:25 +0530

    grooveiscomming

---
+++ app.js
@@ -0,0 +1,9 @@
+var placeorder = document.getElementById("placeorder")
+var thanktext = document.getElementById("thanktext")
+
+
+placeorder.onclick=()=>{
+    thanktext.style.display="block"
+
+    placeorder.style.display="none"
+}
